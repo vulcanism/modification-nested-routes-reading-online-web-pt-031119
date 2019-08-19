f505c52208160b1aeb578c9438aa05437b3dc49e
Savannah C <sgcorrell@gmail.com>
2019-08-19 15:59:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :authors, only: [:show, :index] do
-    resources :posts, only: [:show, :index, :new]
+    resources :posts, only: [:show, :index, :new, :edit]
   end
 end
